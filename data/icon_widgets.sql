4cdb46bc8a569048e27c57395f6d38b04ecef53a
Votilarus <2804119807@qq.com>
2026-05-09 16:41:07 +0800

    创建组件表

---
+++ data/icon_widgets.sql
@@ -0,0 +1,44 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : SQLite-Votilarus
+ Source Server Type    : SQLite
+ Source Server Version : 3035005
+ Source Schema         : main
+
+ Target Server Type    : SQLite
+ Target Server Version : 3035005
+ File Encoding         : 65001
+
+ Date: 09/05/2026 16:37:41
+*/
+
+PRAGMA foreign_keys = false;
+
+-- ----------------------------
+-- Table structure for icon_widgets
+-- ----------------------------
+DROP TABLE IF EXISTS "icon_widgets";
+CREATE TABLE "icon_widgets" (
+  "id" INTEGER NOT NULL,
+  "category_id" integer,
+  "sort_order" integer,
+  "title" TEXT,
+  "pox_x" NUMBER,
+  "pox_y" NUMBER,
+  "width" NUMBER,
+  "height" NUMBER,
+  "active_flag" integer,
+  "created_at" DATE,
+  "updated_at" DATE,
+  "deleted_at" DATE,
+  "deleted_flag" integer,
+  PRIMARY KEY ("id")
+);
+
+-- ----------------------------
+-- Records of icon_widgets
+-- ----------------------------
+INSERT INTO "icon_widgets" VALUES (1, -1, 1, 'ClockWidget', 0, 0, 2, 2, 1, NULL, NULL, NULL, 0);
+
+PRAGMA foreign_keys = true;
